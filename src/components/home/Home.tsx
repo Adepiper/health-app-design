cc35e5bfb5538aba59912b1ef500e49b92777b31
Oluwatobiloba Adebayo <hardebaryorone@gmail.com>
2022-12-17 09:42:45 +0100

    footer done

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,16 +1,62 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Link, Navigate, Route, Routes } from 'react-router-dom';
 import Activity from './activity/Activity';
 import History from './history/History';
+import classes from './Home.module.scss';
 
 const Home = () => {
 	return (
-		<div>
+		<div className={classes.container}>
 			<Routes>
 				<Route path={`activity`} element={<Activity />} caseSensitive />
 				<Route path={`history`} element={<History />} caseSensitive />
 				<Route path='*' element={<Navigate to={'activity'} />} />
 			</Routes>
+			<div className={classes.footerContainer}>
+				<ul>
+					<div>
+						<li>
+							<Link to={''}>
+								<i
+									className='fa fa-address-card-o'
+									aria-hidden='true'
+								></i>
+							</Link>
+						</li>
+						<li>
+							<Link to={''}>
+								<i
+									className='fa fa-address-card-o'
+									aria-hidden='true'
+								></i>
+							</Link>
+						</li>
+					</div>
+
+					<div>
+						<li>
+							<Link to={''}>
+								<i
+									className='fa fa-address-card-o'
+									aria-hidden='true'
+								></i>
+							</Link>
+						</li>
+						<li>
+							<Link to={''}>
+								<i
+									className='fa fa-address-card-o'
+									aria-hidden='true'
+								></i>
+							</Link>
+						</li>
+					</div>
+				</ul>
+
+				<Link className={classes.addWeight} to={''}>
+					<i className='fa fa-plus' aria-hidden='true'></i>
+				</Link>
+			</div>
 		</div>
 	);
 };
