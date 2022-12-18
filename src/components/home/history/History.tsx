97c5cdb55cec028b1a5990f25ed7ffcb393b5b0e
Oluwatobiloba Adebayo <hardebaryorone@gmail.com>
2022-12-18 04:06:08 +0100

    added bmi

--- src/components/home/history/History.tsx
+++ src/components/home/history/History.tsx
@@ -24,7 +24,58 @@ const History = () => {
 					<LineChartGraph />
 				</div>
 			</div>
-			<div className={classes.box}></div>
+			<div className={classes.box}>
+				<div className={classes.bmiHeader}>
+					<h3>BMI</h3>
+
+					<div>
+						<h3>17.3</h3>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							viewBox='0 0 320 512'
+						>
+							<path d='M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8H288c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z' />
+						</svg>
+					</div>
+				</div>
+
+				<div className={classes.progressContainer}>
+					<div className={classes.progressone}>
+						<div></div>
+					</div>
+					<div className={classes.progresstwo}>
+						<div></div>
+					</div>
+					<div className={classes.progressthree}>
+						<div></div>
+					</div>
+				</div>
+
+				<div className={classes.progressBarNumber}>
+					<div>
+						<p className={classes.firstPointer}>18,5</p>
+					</div>
+					<div></div>
+					<div>
+						<p className={classes.lastPointer}>25</p>
+					</div>
+				</div>
+
+				<div className={classes.progressLabel}>
+					<div className={classes.label}>
+						<span className={classes.low}></span>
+						<p>Low</p>
+					</div>
+					<div className={classes.label}>
+						<span className={classes.normal}></span>
+						<p>Normal</p>
+					</div>
+					<div className={classes.label}>
+						<span className={classes.high}></span>
+						<p>high</p>
+					</div>
+				</div>
+			</div>
 			<div className={classes.box}></div>
 		</div>
 	);
